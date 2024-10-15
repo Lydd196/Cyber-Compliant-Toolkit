728e2adb542d7f96878141149e89f77c6639e0a2
Lydd196 <oliveredwards196@gmail.com>
2024-10-15 12:14:59 +0100

    cleaned up code, better var names + comments etc

--- main.py
+++ main.py
@@ -4,27 +4,30 @@ import random
 import questionhandler 
 
 #Create the main window
-root = ctk.CTk()
-root.title("Cyber Law Compliance Toolkit")
-root.geometry("1300x650")
+window = ctk.CTk()
+window.title("Cyber Law Compliance Toolkit")
+window.geometry("1300x650")
 
 #Initial compliance level as a percentage
 complianceLevel = 100
 
+#Function to clear previous question elements
+def clearElements(window):
+    for elements in window.winfo_children():
+        elements.destroy() 
+
 def showResults():
     #Remove all the elements to prepare the results and change window title
-    for elements in root.winfo_children():
-        elements.destroy()  
-    root.title("Your Results")
+    clearElements(window)
+    window.title("Your Results")
 
     #Display final compliance value as a percentage with text depending on the final value itself
-    complianceTitleLabel = ctk.CTkLabel(root, text= "Final compliance level:", font=titleFont)        
+    complianceTitleLabel = ctk.CTkLabel(window, text= "Final compliance level:", font=titleFont)        
     complianceTitleLabel.pack(pady=15)
-    
-    complianceLevelLabel = ctk.CTkLabel(root, text=str(complianceLevel) + "%", font=normalFont)
+    complianceLevelLabel = ctk.CTkLabel(window, text=str(complianceLevel) + "%", font=normalFont)
     complianceLevelLabel.pack(pady=15)
 
-    resultDescriptionLabel = ctk.CTkLabel(root, text= "sample text", font=normalFont)
+    resultDescriptionLabel = ctk.CTkLabel(window, text= "sample text", font=normalFont)
     if complianceLevel > 80:
         resultDescriptionLabel.configure(text= "We believe that your business is very compliant with cyber laws. Great Job!")
         resultDescriptionLabel.pack(pady=15)
@@ -35,14 +38,13 @@ def showResults():
         resultDescriptionLabel.configure(text= "We believe that you may be in serious breach of the GDPR.\nYou may face fines up to £17.5 million or 4% of global annual turnover (whichever is higher)")
         resultDescriptionLabel.pack(pady=15)
         
-    endButton = ctk.CTkButton(root, text="End", command=close, font=normalFont)
+    endButton = ctk.CTkButton(window, text="End", command=close, font=normalFont)
     endButton.pack(pady=15)
 
 #Start function for running the questions (starting the test)
 def start():
-    #Remove all the elements to prepare the test
-    for elements in root.winfo_children():
-        elements.destroy()  
+    #Clear all elements from the window to prepare the test
+    clearElements(window) 
     global complianceLevel
     questionNumber = 1
     
@@ -54,7 +56,7 @@ def start():
     #Run each question dynamically
     while questionNumber <= len(questionList):
         question = questionList[questionNumber - 1]
-        complianceLevel = questionhandler.showQuestion(root, question, complianceLevel, questionNumber, questionAmount)
+        complianceLevel = questionhandler.showQuestion(window, question, complianceLevel, questionNumber, questionAmount)
         #Print compliance level in terminal (debugging purposes)
         print("Compliance Level after question", questionNumber, ":", complianceLevel)
         questionNumber = questionNumber + 1
@@ -62,29 +64,30 @@ def start():
     #After each of the questions have been answered, show the results
     showResults()
 
-    
 #Cancel function to not run the test and to close the program
 def close():
-    root.destroy()
+    window.destroy()
 
+#Set fonts
 titleFont = ctk.CTkFont(family="Helvetic", size=25, weight="bold") 
 normalFont = ctk.CTkFont(family="Times New Roman", size=18)
 
-titleLabel= ctk.CTkLabel(root, text="Welcome to CyberComply: The Cyber Law Compliance Toolkit for Accountants", font=titleFont)
+#Set the title
+titleLabel= ctk.CTkLabel(window, text="Welcome to CyberComply: The Cyber Law Compliance Toolkit for Accountants", font=titleFont)
 titleLabel.pack(pady=25)
 
-descriptionLabel = ctk.CTkLabel(root, text="Find out how compliant your accountancy business is with cyber laws", font=normalFont)
+#Set the description
+descriptionLabel = ctk.CTkLabel(window, text="Find out how compliant your accountancy business is with cyber laws", font=normalFont)
 descriptionLabel.pack(pady=15)
 
 #Create start and close buttons
-startButton = ctk.CTkButton(root, text="Start", command=start, font=normalFont)
+startButton = ctk.CTkButton(window, text="Start", command=start, font=normalFont)
 startButton.pack(pady=15)
-
-closeButton = ctk.CTkButton(root, text="Close", command=close, font=normalFont)
+closeButton = ctk.CTkButton(window, text="Close", command=close, font=normalFont)
 closeButton.pack(pady=15)
 
 #Run the main loop
-root.mainloop()
+window.mainloop()
 
 #Print final compliance score in the terminal (debugging purposes)
 print("Final compliance level:", complianceLevel)
--- questionhandler.py
+++ questionhandler.py
@@ -2,63 +2,19 @@ import customtkinter as ctk
 import tkinter as tk
 import json
 
+#Initialise noSelect so it can be accessed by functions
 noSelect = None
+
+#Function to clear previous question elements
+def clearElements(window):
+    for elements in window.winfo_children():
+        elements.destroy() 
+
 #Function to load questions from a json file
 def loadQuestions(questionFile):
     with open(questionFile, 'r') as file:
         questionData = json.load(file)
     return questionData['questions']
-
-#Function to show a question dynamically based on the loaded json file data
-def showQuestion(root, questionData, compliance, questionNumber, questionAmount):
-    global noSelect
-    noSelect = False
-
-    #Clear previous question elements
-    for elements in root.winfo_children():
-        elements.destroy()  
-    root.title("Question " + str(questionNumber))
-
-    questionFont = ctk.CTkFont(family="Helvetic", size=25, weight="bold")
-    normalFont = ctk.CTkFont(family="Times New Roman", size=18)
-
-    #Set question number
-    questionNumberLabel = ctk.CTkLabel(root, text=f"Question {questionNumber}/{questionAmount}", font=normalFont)
-    questionNumberLabel.place(relx=0.95, rely=0.03, anchor=tk.E)
-
-    #Set question text from json file
-    questionLabel = ctk.CTkLabel(root, text=questionData['text'], font=questionFont)
-    questionLabel.pack(pady=30)
-
-    selectedOption = tk.IntVar()
-
-    #Dynamically add answers as radio buttons
-    optionNumber = 0
-    while optionNumber < len(questionData['options']):
-        answerRadioButton = ctk.CTkRadioButton(root, text=questionData['options'][optionNumber], font=normalFont, variable=selectedOption, value=optionNumber + 1)
-        answerRadioButton.pack(pady=10)
-        optionNumber = optionNumber + 1
-
-    #Define submit button functionality, if no option is selected it notifies the user. If an option is selected the compliance is updated and moves onto next question
-    def submit():
-        global newCompliance, noSelect
-        selected = selectedOption.get()
-        if selected == 0 and noSelect == False:
-            noSelectLabel = ctk.CTkLabel(root, text="Please select an option")
-            noSelectLabel.pack(pady=7.5)
-            noSelect = True
-        elif selected != 0:
-            newCompliance = updateCompliance(compliance, selected, questionData['deduction'])
-            root.quit() 
-
-    #Create submit button
-    submitButton = ctk.CTkButton(root, text="Submit", command=submit, font=normalFont)
-    submitButton.pack(pady=15)
-
-    root.mainloop()
-
-    #Updated compliance value is returned
-    return newCompliance
 
 #Function to update compliance level based on user input for each question
 def updateCompliance(compliance, selectedOption, deduction):
@@ -66,3 +22,56 @@ def updateCompliance(compliance, selectedOption, deduction):
     if compliance < 0:
         compliance = 0
     return compliance
+
+#Function to show a question dynamically based on the loaded json file data
+def showQuestion(window, questionData, compliance, questionNumber, questionAmount):
+    global noSelect
+    noSelect = False
+
+    #Remove all the elements to prepare the next question and change window title
+    clearElements(window)
+    window.title("Question " + str(questionNumber))
+
+    #Set fonts
+    questionFont = ctk.CTkFont(family="Helvetic", size=25, weight="bold")
+    normalFont = ctk.CTkFont(family="Times New Roman", size=18)
+
+    #Set question number
+    questionNumberLabel = ctk.CTkLabel(window, text="Question " + str(questionNumber) + "/" + str(questionAmount), font=normalFont)
+    questionNumberLabel.place(relx=0.95, rely=0.02, anchor=tk.E)
+
+    #Set question text from json file
+    questionLabel = ctk.CTkLabel(window, text=questionData['text'], font=questionFont)
+    questionLabel.pack(pady=30)
+
+    #The selected option is stored as an int, first radiobutton is value 1, second radiobutton is value 2, etc.
+    selectedOption = tk.IntVar()
+
+    #Dynamically add answers as radio buttons
+    optionNumber = 1
+    while optionNumber <= len(questionData['options']):
+        answerRadioButton = ctk.CTkRadioButton(window, text=questionData['options'][optionNumber-1], font=normalFont, variable=selectedOption, value=optionNumber)
+        answerRadioButton.pack(pady=10)
+        optionNumber = optionNumber + 1
+
+    #Define submit button functionality, if no option is selected it notifies the user. If an option is selected the compliance is updated and moves onto next question
+    def submit():
+        global newCompliance, noSelect
+        optionSelected = selectedOption.get()
+        if optionSelected == 0 and noSelect == False:
+            noSelectLabel = ctk.CTkLabel(window, text="Please select an option")
+            noSelectLabel.pack(pady=7.5)
+            noSelect = True
+        elif optionSelected != 0:
+            newCompliance = updateCompliance(compliance, optionSelected, questionData['deduction'])
+            window.quit() 
+
+    #Create submit button
+    submitButton = ctk.CTkButton(window, text="Submit", command=submit, font=normalFont)
+    submitButton.pack(pady=15)
+    
+    #Run the main loop
+    window.mainloop()
+
+    #Updated compliance value is returned
+    return newCompliance
